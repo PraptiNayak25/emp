086b6c61d377fba940a2bd4fcff2278d165afd4c
prapticnayak@gmail.com <prapticnayak@gmail.com>
2025-05-07 19:28:07 +0530

    save

---
+++ Basic.java
@@ -0,0 +1,16 @@
+public class Basic{
+    
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+
+       
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+    }
+}
